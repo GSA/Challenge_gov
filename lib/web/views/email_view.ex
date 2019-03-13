d9c608b8d11dca555b25c8a6c33bfa126b9efb83
Eric Oestrich <eric@oestrich.org>
2019-03-13 15:25:48 -0400

    Handle password resets
    - Send a reset email with a link to reset their password
    - Verify any of the signed out controllers are dealt with while signed
      out

--- lib/web/views/email_view.ex
+++ lib/web/views/email_view.ex
@@ -3,6 +3,10 @@ defmodule Web.EmailView do
 
   alias Web.Endpoint
 
+  def password_reset_url(user) do
+    Routes.registration_reset_url(Endpoint, :edit, token: user.password_reset_token)
+  end
+
   def verification_url(user) do
     Routes.registration_verify_url(Endpoint, :show, token: user.email_verification_token)
   end
