a6e4ddf755a4b3c5902b5a3373fc6d53eeb3ed0d
Eric Oestrich <eric@oestrich.org>
2019-03-26 12:24:42 -0400

    Use the hosted version of stein

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule IdeaPortal.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:plug_cowboy, "~> 2.0"},
       {:postgrex, ">= 0.0.0"},
-      {:stein, git: "https://github.com/smartlogic/stein.git", branch: "storage"}
+      {:stein, "~> 0.3"}
     ]
   end
 
