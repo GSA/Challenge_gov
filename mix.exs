e959655cee2d0ef71f1b223d806d7f5d5c1bc6e3
Eric Oestrich <eric@oestrich.org>
2019-03-26 15:09:21 -0400

    Use bamboo master, and update stein

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule IdeaPortal.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:bamboo, "~> 1.2"},
+      {:bamboo, git: "https://github.com/thoughtbot/bamboo.git"},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:ecto_sql, "~> 3.0"},
       {:elixir_uuid, "~> 1.2"},
