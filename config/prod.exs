fb550c1fbc460cd8bd0da7803dd832b783fc3902
Eric Oestrich <eric@oestrich.org>
2019-05-20 16:10:33 -0400

    Force TLS on heroku

--- config/prod.exs
+++ config/prod.exs
@@ -12,6 +12,7 @@ use Mix.Config
 config :idea_portal, Web.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: System.get_env("HOST"), scheme: "https", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
