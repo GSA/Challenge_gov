65afb8d3b2a556e2402c58b334ed15d207caa6e4
Stephanie Vizzi <stephanie@smartlogic.io>
2020-02-10 19:58:39 -0500

    redirect and hidden items on post login form

--- lib/web/controllers/session_controller.ex
+++ lib/web/controllers/session_controller.ex
@@ -112,19 +112,15 @@ defmodule Web.SessionController do
 
   Or the home page
   """
-  def after_sign_in_redirect(conn, default_path, user) do
-    if user.role == "admin" do
-      redirect(conn, to: Routes.admin_challenge_path(conn, :index))
-    else
-      case get_session(conn, :last_path) do
-        nil ->
-          redirect(conn, to: default_path)
+  def after_sign_in_redirect(conn, default_path) do
+    case get_session(conn, :last_path) do
+      nil ->
+        redirect(conn, to: default_path)
 
-        path ->
-          conn
-          |> put_session(:last_path, nil)
-          |> redirect(to: path)
-      end
+      path ->
+        conn
+        |> put_session(:last_path, nil)
+        |> redirect(to: path)
     end
   end
 end
--- lib/web/views/admin/terms_view.ex
+++ lib/web/views/admin/terms_view.ex
@@ -2,4 +2,36 @@ defmodule Web.Admin.TermsView do
   use Web, :view
 
   alias Web.Admin.FormView
+
+  def challenge_owner_fields(f, user) do
+    if user.data.role == "challenge_owner" do
+      [
+        content_tag(:div, class: "input") do
+          [
+            label(f, :first_name, "First Name*", class: "label-text"),
+            text_input(f, :first_name, required: true, placeholder: "First Name", class: "form-control fc-input")
+          ]
+        end,
+        content_tag(:div, class: "input") do
+          [
+            label(f, :last_name, "Last Name*", class: "label-text"),
+            text_input(f, :last_name, required: true, placeholder: "Last Name", class: "form-control fc-input")
+          ]
+        end,
+        content_tag(:div, class: "input") do
+          [
+            label(f, :email, "Email Address*", class: "label-text"),
+            text_input(f, :email, required: true, placeholder: "Email Address", class: "form-control fc-input")
+          ]
+        end,
+        content_tag(:div, class: "input") do
+          [
+            label(f, :agency_id, "Agency Name*", class: "label-text"),
+            text_input(f, :agency_id, required: true, placeholder: "Agency Name", class: "form-control fc-input")
+          ]
+        end,
+      ]
+    end
+  end
+
 end
