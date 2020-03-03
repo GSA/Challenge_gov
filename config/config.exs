996cdbf44b1fad5dc0b666be2e0084f4ad5b977f
Homerade <Homerade@users.noreply.github.com>
2020-03-03 12:35:30 -0500

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -31,10 +31,7 @@ config :challenge_gov, :recaptcha, module: ChallengeGov.Recaptcha.Implementation
 
 config :stein_storage,
   backend: :file,
-  file_backend_folder: "uploads/"
-  
-# Sets the session timeout interval in minutes	
-config :challenge_gov, :session_timeout_in_minutes, 15  
+  file_backend_folder: "uploads/" 
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
