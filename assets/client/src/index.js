bd3a16c843450a7a4014f7578b7b3a9ad8257950
Stephanie Vizzi <stephanie@smartlogic.io>
2020-03-10 12:10:11 -0400

    react router

--- assets/client/src/index.js
+++ assets/client/src/index.js
@@ -3,9 +3,35 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import '../../css/sass/main.scss'
 import App from './App';
+import { BrowserRouter, Redirect, Switch, Route } from "react-router-dom";
+import { IndexRoutes } from "./routes/index";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const getRoutes = () => {
+  return IndexRoutes.map((prop, i) => {
+    if (prop.redirect) {
+      return (
+        <Redirect from={prop.path} to={prop.pathTo} key={`route-${i}`} />
+      );
+    }
+    return (
+      <Route
+        path={prop.path}
+        key={`route-${i}`}
+        component={prop.component}
+        exact={prop.exact && true}
+      />
+    );
+  });
+};
+
+const renderRouter = () => (
+  <BrowserRouter>
+    <Switch>{getRoutes()}</Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(renderRouter(), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
