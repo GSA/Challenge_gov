2f5122dea887757362ce1d9d3f8ac516d3dc1484
Stephanie Vizzi <stephanie@smartlogic.io>
2020-03-23 17:09:59 -0400

    background task to rm all records 180 days or older

--- lib/challenge_gov/security_logs.ex
+++ lib/challenge_gov/security_logs.ex
@@ -11,4 +11,24 @@ defmodule ChallengeGov.SecurityLogs do
     |> SecurityLog.changeset(user, type, data)
     |> Repo.insert()
   end
+
+  def all() do
+    Repo.all(SecurityLog)
+  end
+
+  def check_expired_records() do
+    Enum.map(__MODULE__.all(), fn record ->
+      remove_expired_records(record)
+    end)
+  end
+
+  def remove_expired_records(record) do
+    inserted_at = Timex.to_unix(record.inserted_at)
+    # 180 days ago
+    expiration_date = DateTime.to_unix(Timex.shift(DateTime.utc_now(), days: -180))
+
+    if expiration_date >= inserted_at do
+      Repo.delete(record)
+    end
+  end
 end
