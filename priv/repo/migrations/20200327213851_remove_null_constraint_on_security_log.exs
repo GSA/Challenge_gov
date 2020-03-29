f67ff4ddea01dbff8e397075968faa60b955b10d
Stephanie Vizzi <stephanie@smartlogic.io>
2020-03-29 19:35:16 -0400

    updated security log table

---
+++ priv/repo/migrations/20200327213851_remove_null_constraint_on_security_log.exs
@@ -0,0 +1,21 @@
+defmodule ChallengeGov.Repo.Migrations.RemoveNullConstraintOnSecurityLog do
+  use Ecto.Migration
+
+  def up do
+    drop(constraint(:security_log, "security_log_originator_id_fkey"))
+    alter table (:security_log) do
+      modify :originator_id, references(:users), null: true
+      modify :originator_role, :string, null: true
+      modify :originator_identifyer, :string, null: true
+    end
+  end
+
+  def down do
+    drop(constraint(:security_log, "security_log_originator_id_fkey"))
+    alter table (:security_log) do
+      modify :originator_id, references(:users), null: true
+      modify :originator_role, :string, null: true
+      modify :originator_identifyer, :string, null: true
+    end
+  end
+end
