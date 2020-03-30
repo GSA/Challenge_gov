f76702051aa8cd093f26a0baa2086eef2692ff4b
Stephanie Vizzi <stephanie@smartlogic.io>
2020-03-30 18:55:55 -0400

    security log updates: single user actions, session duration readability, extra condition on last_accessed_site

--- lib/challenge_gov/security_logs.ex
+++ lib/challenge_gov/security_logs.ex
@@ -36,7 +36,7 @@ defmodule ChallengeGov.SecurityLogs do
   def log_session_duration(user, session_end) do
     last_accessed_site =
       SecurityLog
-      |> where([l], l.target_id == ^user.id)
+      |> where([l], l.originator_id == ^user.id and l.action == "accessed_site")
       |> limit(1)
       |> order_by([l], desc: l.logged_at)
       |> Repo.one()
@@ -45,13 +45,12 @@ defmodule ChallengeGov.SecurityLogs do
 
     duration = session_end - user_accessed_site
 
-    # TODO: convert to more readable time {ISOtime}?
     track(%SecurityLog{}, %{
       action: "session_duration",
       details: %{duration: duration},
-      target_id: user.id,
-      target_type: user.role,
-      target_identifier: user.email
+      originator_id: user.id,
+      originator_role: user.role,
+      originator_identifier: user.email
     })
   end
 end
--- lib/web/views/admin/report_view.ex
+++ lib/web/views/admin/report_view.ex
@@ -26,7 +26,7 @@ defmodule ChallengeGov.Admin.ReportView do
     csv = [
       record.id,
       record.action,
-      format_to_readable(record.details),
+      parse_details(record.details),
       record.originator_id,
       record.originator_role,
       record.originator_identifier,
@@ -39,12 +39,48 @@ defmodule ChallengeGov.Admin.ReportView do
     CSV.dump_to_iodata([csv])
   end
 
-  defp format_to_readable(record) do
+  defp parse_details(record) do
     if record do
       record
       |> Enum.map(fn x ->
-        ["#{elem(x, 0)}: #{elem(x, 1)}"]
+        format_to_readable(x)
       end)
+    end
+  end
+
+  defp format_to_readable(x) do
+    case elem(x, 0) == "duration" do
+      true ->
+        ["#{elem(x, 0)}: #{convert_to_iostime(elem(x, 1))}"]
+
+      false ->
+        ["#{elem(x, 0)}: #{elem(x, 1)}"]
+    end
+  end
+
+  defp convert_to_iostime(duration) do
+    {hours, minutes, seconds, _microseconds} =
+      duration
+      |> Timex.Duration.from_seconds()
+      |> Timex.Duration.to_clock()
+
+    "#{ensure_double_digits(hours)}:#{ensure_double_digits(minutes)}:#{
+      ensure_double_digits(seconds)
+    }"
+  end
+
+  def ensure_double_digits(elem) do
+    result =
+      elem
+      |> Integer.digits()
+      |> length
+
+    case result == 1 do
+      true ->
+        "0#{elem}"
+
+      false ->
+        elem
     end
   end
 end
