4bae0b500cf61bff5f990b3438ad76b8693761ed
Chris Preisinger <cpreisinger@gmail.com>
2020-04-08 00:44:11 -0400

    Added layout around public view
    - Updated navbar/header
    - Added top description banner
    - Added bottom help section
    - Added footer
    - Added white challenge logo
    - Added white square challenge logo

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,5 +22,6 @@ import "phoenix_html"
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 import "./custom.js";
+import "./scroll_to_anchor.js";
 
 window.$ = jquery;
