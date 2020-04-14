04a55e8d706e9a5a9cbde78b27cfdd7f5c5dd527
Stephanie Vizzi <stephanie@smartlogic.io>
2020-04-14 11:27:19 -0400

    react ga on reg and router pages

--- assets/client/src/useTracking.js
+++ assets/client/src/useTracking.js
@@ -1,14 +1,27 @@
 import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import ReactGA from 'react-ga'
 
 export const useTracking = (GA_MEASUREMENT_ID) => {
 
+// send pathname on SPA routed pages
+  let location = useLocation()
+
+  useEffect(
+    () => {
+      ReactGA.ga('send', 'pageview', location.pathname);
+    },
+    [location]
+  )
+
+// inject script tag into all other pages
   useEffect(() => {
     const script = document.createElement("script");
 
     script.async = true;
     script.type = "text/javascript"
     script.id = GA_MEASUREMENT_ID
-    script.src = "https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js";
+    script.src = "https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=GSA";
 
     document.body.appendChild(script)
   },[])
