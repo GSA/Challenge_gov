fb5f424d15e6a726464585f4247cca20ce073b87
Homerade <Homerade@users.noreply.github.com>
2020-05-29 10:16:17 -0400

    Delete 20200526161207_add_external_challenge_logo_url.exs

--- priv/repo/migrations/20200526161207_add_external_challenge_logo_url.exs
+++
@@ -1,9 +0,0 @@
-defmodule ChallengeGov.Repo.Migrations.AddExternalChallengeLogoUrl do
-  use Ecto.Migration
-
-  def change do
-    alter table(:challenges) do
-      add(:external_logo_url, :string)
-    end
-  end
-end
