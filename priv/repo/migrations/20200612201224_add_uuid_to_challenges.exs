fe33660ea76818cb228611730352ed5bced3c874
Chris Preisinger <cpreisinger@gmail.com>
2020-06-15 10:42:23 -0400

    Switch to using Ecto to generate challenge UUIDs

--- priv/repo/migrations/20200612201224_add_uuid_to_challenges.exs
+++ priv/repo/migrations/20200612201224_add_uuid_to_challenges.exs
@@ -2,16 +2,14 @@ defmodule ChallengeGov.Repo.Migrations.AddUuidToChallenges do
   use Ecto.Migration
 
   def up do
-    execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
-
     alter table(:challenges) do
-      add(:uuid, :uuid, default: fragment("uuid_generate_v4()"))
+      add(:uuid, :uuid, default: Ecto.UUID.generate())
     end
   end
 
   def down do
     alter table(:challenges) do
-      remove(:uuid, :uuid, default: fragment("uuid_generate_v4()"))
+      remove(:uuid, :uuid)
     end
   end
 end
