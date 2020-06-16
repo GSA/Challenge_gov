2d6a8e7ba9c53cc171ebea6ddd7f4f5830e3f882
Chris Preisinger <cpreisinger@gmail.com>
2020-06-15 21:12:20 -0400

    Added html rendering to challenge details FAQ

--- assets/client/src/components/challenge_tabs/FAQ.js
+++ assets/client/src/components/challenge_tabs/FAQ.js
@@ -4,7 +4,7 @@ import { ChallengeTab } from "../ChallengeTab"
 export const FAQ = ({challenge}) => {
   return (
     <ChallengeTab label="Frequently asked questions" downloadsLabel="Additional FAQ documents" section="faq" challenge={challenge}>
-      {challenge.faq}
+      <div dangerouslySetInnerHTML={{ __html: challenge.faq }}></div>
     </ChallengeTab>
   )
 }
--- assets/client/src/components/challenge_tabs/Prizes.js
+++ assets/client/src/components/challenge_tabs/Prizes.js
@@ -5,7 +5,7 @@ export const Prizes = ({challenge}) => {
   return (
     <ChallengeTab label="Prizes" downloadsLabel="Additional prize documents" section="prizes" challenge={challenge}>
       <div>{challenge.prize_total}</div>
-      <div dangerouslySetInnerHTML={{ __html: challenge.non_monetary_prizes }}></div>
+      <div>{challenge.non_monetary_prizes}</div>
       <div dangerouslySetInnerHTML={{ __html: challenge.prize_description }}></div>
     </ChallengeTab>
   )
