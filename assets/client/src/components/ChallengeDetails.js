ded37360b414ba524e0925d8471b6a2919e60e42
Dan Ivovich <divovich@gmail.com>
2020-09-27 07:24:08 -0400

    Show subscriber count on follow button and to owner

--- assets/client/src/components/ChallengeDetails.js
+++ assets/client/src/components/ChallengeDetails.js
@@ -68,7 +68,7 @@ export const ChallengeDetails = ({challenge, preview}) => {
             <h4>Follow challenge as guest</h4>
             <p>Receive challenge updates to your email. No sign-in required</p>
             <a href={preview ? null : challenge.gov_delivery_topic_subscribe_link}>
-              <button className="follow-tooltip__button">Follow challenge</button>
+              <button className="follow-tooltip__button">Follow challenge ({ challenge.gov_delivery_subscriber_count })</button>
             </a>
           </div>
           <div className="follow-tooltip__divider">Or</div>
