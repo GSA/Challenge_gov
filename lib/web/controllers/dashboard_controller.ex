232060f5356d8d14fe225c6f1a3af4a7779821fa
Austin Lopez <austin.lopez@gsa.gov>
2021-02-15 12:31:22 -0500

    delete inspects

--- lib/web/controllers/dashboard_controller.ex
+++ lib/web/controllers/dashboard_controller.ex
@@ -2,15 +2,8 @@ defmodule Web.DashboardController do
   use Web, :controller
 
   def index(conn, _params) do
-    IO.inspect("CONN")
-    IO.inspect(conn)
-    IO.inspect("question: why isn't the conn layout root correct?")
     conn = put_root_layout(conn, {Web.LayoutView, :root})
-    conn = put_new_layout(conn, {Web.LayoutView, :root})
-    IO.inspect("NEW CONN")
-    IO.inspect(conn)
     %{current_user: user} = conn.assigns
-    # redirect(conn, to: Routes.challenge_path(conn, :index))
     conn
     |> assign(:user, user)
     |> assign(:filter, nil)
