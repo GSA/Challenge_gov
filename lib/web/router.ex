8fae97c010cb98ea59e2fff9d8fa99de06b8e64c
Austin Lopez <austin.lopez@gsa.gov>
2021-02-15 16:23:12 -0500

    upload image buttons

--- lib/web/router.ex
+++ lib/web/router.ex
@@ -90,8 +90,9 @@ defmodule Web.Router do
 
       resources("/bulletin", BulletinController, only: [:new, :create])
 
-        get("/winners/publish", PhaseController, :winners_published, as: "winner")
+
       resources("/phases", PhaseController, only: [:index, :show]) do
+        get("/winners_published", PhaseController, :winners_published, as: "winner")
         post("/winners", PhaseController, :create_winners, as: "winner")
 
         resources("/solutions", SolutionController, only: [:index, :show, :new, :create])
