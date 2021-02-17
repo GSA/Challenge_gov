caf32808d5a9c43739ff790f8fcbddf463b24915
Austin Lopez <austin.lopez@gsa.gov>
2021-02-17 17:39:11 -0500

    delete extra code 'winners'

--- lib/web/controllers/phase_controller.ex
+++ lib/web/controllers/phase_controller.ex
@@ -99,35 +99,4 @@ defmodule Web.PhaseController do
         |> redirect(to: Routes.challenge_path(conn, :index))
     end
   end
-
-  def create_winners(conn, %{"challenge_id" => cid, "phase_id" => pid} = params) do
-    challenge = Challenges.get(cid)
-
-    conn
-    |> assign(:challenge, challenge)
-  end
-
-  def winners(conn, %{"challenge_id" => cid, "phase_id" => phid} = params) do
-    {:ok, challenge} = Challenges.get(cid)
-    {:ok, phase} = Phases.get(phid)
-    changeset = Phase.changeset(phase, %{})
-
-    conn
-    |> assign(:changeset, changeset)
-    |> assign(:challenge_id, cid)
-    |> assign(:phase_id, phid)
-    |> assign(:challenge, challenge)
-    |> assign(:phase, phase)
-    |> render("winners.html")
-  end
-
-  def winners_published(conn, %{"challenge_id" => cid, "phase_id" => pid} = params) do
-    {:ok, phase} = Phases.get(pid)
-    IO.inspect("PHASE THEN CHANGESET")
-    changeset = Phase.changeset(phase, %{}) |> IO.inspect()
-
-    conn
-    |> assign(:changeset, changeset)
-    |> render("winners_published.html")
-  end
 end
