fe33da2df351304970a664bdd48bd1d600b2463f
Austin Lopez <austin.lopez@gsa.gov>
2021-02-17 17:04:50 -0500

    linting

--- lib/web.ex
+++ lib/web.ex
@@ -36,7 +36,7 @@ defmodule Web do
         pattern: "**/*"
 
       import Phoenix.Controller,
-      only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
+        only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
       unquote(view_helpers())
     end
