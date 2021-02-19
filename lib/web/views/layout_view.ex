dcdba9a440e6d908fd96bf40085c3b42692e9bd2
Austin Lopez <austin.lopez@gsa.gov>
2021-02-18 21:05:38 -0500

    undo commas

--- lib/web/views/layout_view.ex
+++ lib/web/views/layout_view.ex
@@ -83,14 +83,14 @@ defmodule Web.LayoutView do
 end
 
 defmodule Web.PageTitle do
-  alias Web.Admin,
-    alias Web.SessionView,
-    alias Web.RegistrationView,
-    alias Web.RegistrationResetView,
-    alias Web.UserInviteView,
-    alias Web.ChallengeView,
-    alias Web.AccountView,
-    alias Web.AgencyView,
+  alias Web.Admin
+    alias Web.SessionView
+    alias Web.RegistrationView
+    alias Web.RegistrationResetView
+    alias Web.UserInviteView
+    alias Web.ChallengeView
+    alias Web.AccountView
+    alias Web.AgencyView
     alias Web.ErrorView
 
   @app_name "Challenge.gov"
