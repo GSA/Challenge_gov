4894631c983109c336e8328aed9c2f2342f62eaf
Stephanie Vizzi <stephanie@smartlogic.io>
2021-04-02 13:09:38 -0400

    bugfix: remove back button from general step
    desc: if hit back from general section it breaks
    
    location: changeset_for_action > draft_changeset > to_section
    
    fix: remove back button from general section
    
    alt: make back redirect to challenges list
    
    templates/challenge/wizard.html.eex
    * add if section != "general around back button

--- lib/web/views/challenge_view.ex
+++ lib/web/views/challenge_view.ex
@@ -424,15 +424,17 @@ defmodule Web.ChallengeView do
     end
   end
 
-  def back_button(conn, challenge) do
-    if challenge.id do
-      submit("Back", name: "action", value: "back", class: "btn btn-link", formnovalidate: true)
-    else
-      link("Back",
-        to: Routes.challenge_path(conn, :index),
-        class: "btn btn-link",
-        formnovalidate: true
-      )
+  def back_button(conn, challenge, section) do
+    if section != Enum.at(Challenges.sections(), 0).id do
+      if challenge.id do
+        submit("Back", name: "action", value: "back", class: "btn btn-link", formnovalidate: true)
+      else
+        link("Back",
+          to: Routes.challenge_path(conn, :index),
+          class: "btn btn-link",
+          formnovalidate: true
+        )
+      end
     end
   end
 
