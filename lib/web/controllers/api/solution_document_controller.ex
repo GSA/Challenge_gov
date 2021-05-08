890f698d64c9fa84c9a92db7096af50988a16ac7
Stephanie Vizzi <stephanie@smartlogic.io>
2021-05-08 15:24:15 -0400

    PR review change: refactor nested case into a with

--- lib/web/controllers/api/solution_document_controller.ex
+++ lib/web/controllers/api/solution_document_controller.ex
@@ -21,22 +21,19 @@ defmodule Web.Api.SolutionDocumentController do
   end
 
   def create(conn, %{"document" => params, "solver_email" => solver_email}) do
-    case Accounts.get_by_email(solver_email) do
-      {:ok, user} ->
-        case SolutionDocuments.upload(user, params) do
-          {:ok, document} ->
-            conn
-            |> assign(:document, document)
-            |> put_status(:created)
-            |> render("show.json")
-
-          {:error, changeset} ->
-            conn
-            |> assign(:changeset, changeset)
-            |> put_status(:unprocessable_entity)
-            |> put_view(ErrorView)
-            |> render("errors.json")
-        end
+    with {:ok, user} <- Accounts.get_by_email(solver_email),
+         {:ok, document} <- SolutionDocuments.upload(user, params) do
+      conn
+      |> assign(:document, document)
+      |> put_status(:created)
+      |> render("show.json")
+    else
+      {:error, changeset} ->
+        conn
+        |> assign(:changeset, changeset)
+        |> put_status(:unprocessable_entity)
+        |> put_view(ErrorView)
+        |> render("errors.json")
 
       {:error, :not_found} ->
         {:error, changeset} =
