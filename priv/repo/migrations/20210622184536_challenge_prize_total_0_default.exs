f6cb1fcd8f66f7fcfa20493fa8efe9b4b1d26058
Dan Ivovich <daniel.ivovich@gsa.gov>
2021-06-23 13:42:26 -0400

    Update 20210622184536_challenge_prize_total_0_default.exs
    update before setting not null

--- priv/repo/migrations/20210622184536_challenge_prize_total_0_default.exs
+++ priv/repo/migrations/20210622184536_challenge_prize_total_0_default.exs
@@ -2,10 +2,9 @@ defmodule ChallengeGov.Repo.Migrations.ChallengePrizeTotal0Default do
   use Ecto.Migration
 
   def change do
+    execute "update challenges set prize_total = 0 where prize_total = null;", ""
     alter table(:challenges) do
       modify(:prize_total, :integer, null: false)
     end
-
-    execute "update challenges set prize_total = 0 where prize_total = null;"
   end
 end
