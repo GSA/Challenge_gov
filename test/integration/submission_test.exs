1db53bcf2c03698c3b809792127fa0a49769ba84
Stephanie Vizzi <stephanie@smartlogic.io>
2021-08-01 21:44:27 -0400

    Admin created submission test
    submission_test.exs
    * admin creates submission
    * confirm submission not reviewed/verified by solver
    * confirm submission does not appear in list to be judged
    * confirm submission does appear in Solver submissions
    
    submission_view.ex
    * update managed submissions table to show "no"/"yes" in Accepted by
    solver column
    
    index_managed.html.eex
    * add id to review verified table column to assert value in test
    
    dev_accounts.html.eex
    * update dev account button text to allow test to find and press

--- test/integration/submission_test.exs
+++ test/integration/submission_test.exs
@@ -13,24 +13,99 @@ defmodule ChallengeGov.SubmissionTest do
     |> click(link("#{challenge.title}"))
     |> click(link("Apply for this challenge"))
     |> fill_in(text_field("Title"), with: "Test Submission")
-    |> execute_script("document.getElementsByClassName('ql-editor')[0].innerHTML = 'okay'")
-    |> execute_script("document.getElementsByClassName('ql-editor')[1].innerHTML = 'here. now. please.'")
+    |> execute_script(
+      "document.getElementsByClassName('ql-editor')[0].innerHTML = 'Brief desciption here.'"
+    )
+    |> execute_script(
+      "document.getElementsByClassName('ql-editor')[1].innerHTML = 'Full description here.'"
+    )
     |> click(checkbox("submission[terms_accepted]"))
     |> click(button("Review and submit"))
     |> click(link("Submit"))
     |> assert_text("Submission saved")
 
-    submission_id = current_url(session) |> String.slice(-2..-1)
+    submission_id =
+      session
+      |> current_url()
+      |> String.slice(-2..-1)
 
     session
     |> click(link("< Back to submissions"))
     |> assert_text("#{submission_id}")
   end
 
+  feature "create a submission as an admin", %{session: session} do
+    AccountHelpers.create_user(%{email: "solver_active@example.com", role: "solver"})
+    create_challenge()
+    create_and_sign_in_admin(session)
+
+    session
+    |> click(link("Challenge management"))
+    |> click(link("View"))
+    |> click(link("View submissions"))
+    |> click(link("Add solver submission ->"))
+    |> click(link("New solver submission"))
+    |> fill_in(text_field("Title"), with: "Test Submission")
+    |> execute_script(
+      "document.getElementsByClassName('ql-editor')[0].innerHTML = 'Brief desciption here.'"
+    )
+    |> execute_script(
+      "document.getElementsByClassName('ql-editor')[1].innerHTML = 'Full description here.'"
+    )
+    |> click(button("Review and submit"))
+    |> click(link("Submit"))
+    |> assert_text("Submission saved")
+
+    # Submission is not found in table to be judged
+    submission_id =
+      session
+      |> current_url()
+      |> String.slice(-2..-1)
+
+    session
+    |> click(link("< Back to submissions"))
+
+    !has_text?(session, submission_id)
+
+    # Admin submission table shows the "no" in review verified column
+    session
+    |> click(link("Add solver submission ->"))
+    |> assert_text("Test Submission")
+    |> assert_text(submission_id)
+    |> find(css("#review-verified"))
+    |> has_value?("no")
+
+    # Admin created submission is found in Solver submissions
+    session
+    |> click(link("admin_active@example.com"))
+    |> click(link("Logout"))
+    |> visit("/dev_accounts")
+    |> click(button("Solver Active"))
+    |> assert_text("New submission to review")
+  end
+
   defp create_challenge() do
-    user = AccountHelpers.create_user(%{email: "challenge_owner_active@example.com", role: "challenge_owner"})
-    challenge = ChallengeHelpers.create_single_phase_challenge(user, %{user_id: user.id, custom_url: "test-challenge"})
+    user =
+      AccountHelpers.create_user(%{
+        email: "challenge_owner_active@example.com",
+        role: "challenge_owner"
+      })
+
+    challenge =
+      ChallengeHelpers.create_single_phase_challenge(user, %{
+        user_id: user.id,
+        custom_url: "test-challenge"
+      })
+
     challenge
+  end
+
+  defp create_and_sign_in_admin(session) do
+    AccountHelpers.create_user(%{email: "admin_active@example.com", role: "admin"})
+
+    session
+    |> visit("/dev_accounts")
+    |> click(button("Admin Active"))
   end
 
   defp create_and_sign_in_solver(session) do
