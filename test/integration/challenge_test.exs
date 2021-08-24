d55a6819e44869459b2eaef249c5bc3a365ccd3d
Stephanie Vizzi <stephanie@smartlogic.io>
2021-08-24 08:47:21 -0400

    WIP: challenge integration test

---
+++ test/integration/challenge_test.exs
@@ -0,0 +1,56 @@
+defmodule ChallengeGov.ChallengeTest do
+  use Web.FeatureCase, async: true
+
+  alias ChallengeGov.TestHelpers.AccountHelpers
+
+  feature "create a challenge as a Challenge Manager", %{session: session} do
+    create_and_sign_in_challenge_manager(session)
+    year = String.slice("#{Timex.today.year}", -2..-1)
+
+    session
+    |> click(link("Challenge management"))
+    |> click(link("New"))
+    |> fill_in(text_field("Challenge manager of record"), with: "email@example.com")
+    |> fill_in(text_field("Challenge manager email"), with: "email@example.com")
+    |> fill_in(text_field("Point of contact email"), with: "email@example.com")
+    |> click(select("Lead agency name"))
+    |> click(option("Department of Agriculture"))
+    |> click(select("Sub-agency name"))
+    |> click(option("Agricultural Research Service"))
+    |> fill_in(text_field("Fiscal year"), with: "FY#{year}")
+    |> click(button("Next"))
+    |> fill_in(text_field("Challenge title"), with: "Test Title")
+    |> fill_in(text_field("Tagline"), with: "This is a test")
+    |> find(select("Primary challenge type"))
+    |> click(option("Ideas"))
+    |> execute_script(
+      "document.getElementsByClassName('ql-editor')[0].innerHTML = 'Brief desciption here.'"
+    )
+    |> execute_script(
+      "document.getElementsByClassName('ql-editor')[1].innerHTML = 'Full description here.'"
+    )
+    |> click(radio_button(["challenge[upload_logo]"]))
+    # |> click(Query.text("Upload my own logo", count: 1))
+
+    session
+    |> resize_window(900, 2500)
+    |> take_screenshot()
+
+    # assert .com manager can't submit
+  end
+
+  # then sign in as .gov manager and submit
+
+  defp create_and_sign_in_challenge_manager(session) do
+    AccountHelpers.create_user(%{email: "challenge_manager_active@example.com", role: "challenge_owner"})
+
+    session
+    |> visit("/dev_accounts")
+    |> click(button("Challenge Manager Active"))
+  end
+end
+
+
+# add federal parners
+# |> find(data("child", "federal_partners"))
+# |> click(select("Agency name"), option("Department of Education"))
