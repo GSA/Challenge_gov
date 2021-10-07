d05c2721f5d51498524cfd03dcd02a5a94202a46
stephviz <Homerade@users.noreply.github.com>
2021-10-07 13:12:55 -0400

    Chal 702/js local datetime formatting (#367)
    * Delete node modules and screenshot folders
    
    * Ignore node modules and screenshot folder
    
    * update challenge table start and end dates to local time
    
    * Add format to js local datetime, add fallback if invalid
    
    Co-authored-by: Chris Preisinger <cpreisinger@gmail.com>

--- assets/js/app/_date_conversion.js
+++ assets/js/app/_date_conversion.js
@@ -11,9 +11,9 @@ $(".js-local-date").each(function() {
 $(".js-local-datetime").each(function() {
   let timeZone = moment.tz.guess(true)
   let utc_time = $(this).text()
-  let local_time = moment(utc_time).tz(timeZone).format("MM/DD/YY hh:mm A z")
+  let local_time = moment(utc_time, "MM/DD/YY hh:mm A z").tz(timeZone).format("MM/DD/YY hh:mm A z")
 
-  $(this).text(local_time)
+  if (local_time != "Invalid date") { $(this).text(local_time) }
 })
 
 $(".js-current-local-date").each(function() {
