f249b53e880537989f1840c4cf0d546ada976810
Chris Preisinger <cpreisinger@gmail.com>
2021-10-26 11:47:07 -0400

    Fix typo in eligibility requirements on details

--- assets/client/src/components/challenge_tabs/Rules.js
+++ assets/client/src/components/challenge_tabs/Rules.js
@@ -4,23 +4,25 @@ import { ChallengeTab } from "../ChallengeTab"
 export const Rules = ({challenge, print}) => {
   return (
     <ChallengeTab label="Rules" downloadsLabel="Additional rule documents" section="rules" challenge={challenge} print={print}>
-      <div className="card">
-        <div className="card-body">
-          <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.eligibility_requirementst }}></div>
+      {challenge.eligibility_requirements &&
+        <div className="card">
+          <div className="card-body">
+            <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.eligibility_requirements }}></div>
+          </div>
         </div>
-      </div>
+      }
       <div className="card">
         <div className="card-body">
           <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.rules }}></div>
         </div>
       </div>
-    {!challenge.terms_equal_rules &&
-      <div className="card">
-        <div className="card-body">
-          <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.terms_and_conditions }}></div>
+      {!challenge.terms_equal_rules &&
+        <div className="card">
+          <div className="card-body">
+            <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.terms_and_conditions }}></div>
+          </div>
         </div>
-      </div>
-    }
+      }
     </ChallengeTab>
   )
 }
