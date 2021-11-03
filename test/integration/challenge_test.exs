ee04cee7c7eb1459470a758d6e1bf3f78dd98c49
Stephanie Vizzi <stephanie@smartlogic.io>
2021-11-02 22:45:38 -0400

    Add section identifiers / refactor execute script for functions where possible / abstract & consolidate execute scripts

--- test/integration/challenge_test.exs
+++ test/integration/challenge_test.exs
@@ -2,6 +2,7 @@ defmodule ChallengeGov.ChallengeIntegrationTest do
   use Web.FeatureCase, async: true
 
   alias ChallengeGov.TestHelpers.AccountHelpers
+  alias ChallengeGov.Challenges
 
   feature "create a challenge as a Challenge Manager", %{session: session} do
     create_and_sign_in_challenge_manager(session)
@@ -39,109 +40,89 @@ defmodule ChallengeGov.ChallengeIntegrationTest do
   end
 
   defp complete_details_section(session) do
+    verify_previous_section(:agency_id, 24)
+
     session
     |> fill_in(text_field("Challenge title"), with: "Test Title")
     |> fill_in(text_field("Tagline"), with: "This is a test")
     |> find(select("Primary challenge type"))
     |> click(option("Ideas"))
-    |> execute_script(
-      "document.getElementsByClassName('ql-editor')[0].innerHTML = 'Brief desciption here.'"
-    )
-    |> execute_script(
-      "document.getElementsByClassName('ql-editor')[1].innerHTML = 'Full description here.'"
-    )
-    |> execute_script("document.getElementById('challenge_upload_logo_false').click()")
-    |> execute_script("document.getElementById('challenge_auto_publish_date_picker').focus()")
-    |> execute_script(
-      "document.getElementById('challenge_auto_publish_date_picker').value = '#{publish_date_picker()}'"
-    )
-    |> execute_script(
-      "document.getElementById('challenge_auto_publish_date').value = '#{publish_date()}'"
-    )
+    |> populate_markdown_field("Brief desciption here.")
+    |> populate_markdown_field("Full description here.", 1)
+
+    session
+    |> focus_frame(css(".logo-section"))
+    |> click(radio_button("Use default challenge.gov logo"))
+    |> focus_frame(css(".publish-date-section"))
+    |> populate_auto_publish_date()
 
     # multi-phase false and accept confirm alert
 
     session
+    |> focus_frame(css(".multi-phase-section"))
     |> accept_confirm(fn session ->
-      execute_script(session, "document.getElementById('challenge_is_multi_phase_false').click()")
+      click(session, radio_button("No"))
     end)
 
     # single phase date range
     session
-    |> execute_script("document.getElementById('challenge_phases_0_start_date_picker').focus()")
-    |> execute_script(
-      "document.getElementById('challenge_phases_0_start_date_picker').value = '#{start_date_picker()}'"
-    )
-    |> execute_script(
-      "document.getElementById('challenge_phases_0_start_date').value = '#{start_date()}'"
-    )
-    |> execute_script("document.getElementById('challenge_phases_0_end_date_picker').focus()")
-    |> execute_script(
-      "document.getElementById('challenge_phases_0_end_date_picker').value = '#{end_date_picker()}'"
-    )
-    |> execute_script(
-      "document.getElementById('challenge_phases_0_end_date').value = '#{end_date()}'"
-    )
+    |> populate_start_date("challenge_phases_0_start_date")
+    |> populate_end_date("challenge_phases_0_end_date")
     |> click(button("Next"))
+
   end
 
   defp complete_timeline_section(session) do
+    verify_previous_section(:title, "Test Title")
+
     session
-    |> assert_has(css(".add-nested-section"))
+    |> focus_frame(css(".timeline-event-fields"))
     |> click(css(".add-nested-section"))
     |> fill_in(text_field("Timeline event title"), with: "Event 1")
-    |> execute_script(
-      "document.getElementById('challenge_timeline_events_0_date_picker').focus()"
-    )
-    |> execute_script(
-      "document.getElementById('challenge_timeline_events_0_date_picker').value = '#{end_date_picker()}'"
-    )
-    |> execute_script(
-      "document.getElementById('challenge_timeline_events_0_date').value = '#{end_date()}'"
-    )
+    |> populate_end_date("challenge_timeline_events_0_date")
     |> click(button("Next"))
   end
 
   defp complete_prizes_section(session) do
+    verify_previous_section(:timeline_events, :title, "Event 1")
+
     session
-    |> execute_script("document.getElementById('challenge_prize_type_non_monetary').click()")
+    |> click(radio_button(("Non-monetary prize")))
     |> fill_in(text_field("Non-monetary prize award"), with: "Prizes other than cash")
     |> click(button("Next"))
   end
 
   defp complete_rules_section(session) do
+    verify_previous_section(:non_monetary_prizes, "Prizes other than cash")
+
     session
-    |> execute_script(
-      "document.getElementsByClassName('ql-editor')[0].innerHTML = 'Eligibilty requirements described here.'"
-    )
-    |> execute_script(
-      "document.getElementsByClassName('ql-editor')[1].innerHTML = 'Rules described here.'"
-    )
-    |> execute_script(
-      "document.getElementsByClassName('ql-editor')[2].innerHTML = 'Terms and conditions described here.'"
-    )
+    |> populate_markdown_field("Eligibilty requirements described here.")
+    |> populate_markdown_field("Rules described here.", 1)
+    |> populate_markdown_field("Terms and conditions described here.", 2)
     |> click(select("Legal authority"))
     |> click(option("Direct Prize Authority"))
     |> click(button("Next"))
   end
 
   defp complete_judging_section(session) do
+    verify_previous_section(:legal_authority, "Direct Prize Authority")
+
     session
-    |> execute_script(
-      "document.getElementsByClassName('ql-editor')[0].innerHTML = 'Judging criteria described here.'"
-    )
+    |> populate_markdown_field("Judging criteria described here.")
     |> click(button("Next"))
   end
 
   defp complete_how_to_enter_section(session) do
+    verify_previous_section(:phases, :judging_criteria, "Judging criteria described here.")
+
     session
-    |> execute_script(
-      "document.getElementsByClassName('ql-editor')[0].innerHTML = 'How to enter described here.'"
-    )
+    |> populate_markdown_field("How to enter described here.")
     |> click(button("Next"))
   end
 
   defp complete_resources_section(session) do
+    verify_previous_section(:phases, :how_to_enter, "How to enter described here.")
+
     session
     |> click(button("Next"))
   end
@@ -157,57 +138,85 @@ defmodule ChallengeGov.ChallengeIntegrationTest do
     |> click(button("Challenge Manager Active"))
   end
 
-  defp publish_date_picker() do
-    {:ok, publish_date_picker} =
-      Timex.now()
-      |> Timex.shift(days: 1)
-      |> Timex.format("{YYYY}-{0M}-{0D}T{0h12}:{m}")
-
-    publish_date_picker
+  defp populate_auto_publish_date(session) do
+    session
+    |> execute_script(
+      "document.getElementById('challenge_auto_publish_date_picker').value = '#{
+        set_date_picker(days: 1)
+      }'"
+    )
+    |> execute_script(
+      "document.getElementById('challenge_auto_publish_date').value = '#{set_date(days: 1)}'"
+    )
   end
 
-  defp publish_date() do
-    {:ok, publish_date} =
-      Timex.now()
-      |> Timex.shift(days: 1)
-      |> Timex.format("{YYYY}-{0M}-{0D} {0h12}:{m}:{s}Z")
-
-    publish_date
+  defp populate_start_date(session, id) do
+    session
+    |> execute_script(
+      "document.getElementById('#{id}_picker').value = '#{
+        set_date_picker(days: 2)
+      }'"
+    )
+    |> execute_script(
+      "document.getElementById('#{id}').value = '#{set_date(days: 2)}'"
+    )
   end
 
-  defp start_date_picker() do
+  defp populate_end_date(session, id) do
+    session
+    |> execute_script(
+      "document.getElementById('#{id}_picker').value = '#{
+        set_date_picker(days: 3)
+      }'"
+    )
+    |> execute_script(
+      "document.getElementById('#{id}').value = '#{set_date(days: 3)}'"
+    )
+  end
+
+  defp populate_markdown_field(session, value, index \\ 0) do
+    session
+    |> execute_script(
+      "document.getElementsByClassName('ql-editor')[#{index}].innerHTML = '#{value}'"
+    )
+  end
+
+  defp verify_previous_section(field, value) do
+    challenge = List.first(Challenges.admin_all())
+    key = Map.get(challenge, field)
+    assert(key == value)
+  end
+
+  defp verify_previous_section(field, nested_field, value) do
+    challenge =
+      Challenges.admin_all()
+      |> List.first()
+      |> Map.from_struct()
+
+    key =
+      challenge
+      |> Map.get(field)
+      |> List.first()
+      |> Map.get(nested_field)
+
+    assert(key =~ value)
+  end
+
+  defp set_date_picker(days) do
     {:ok, start_date_picker} =
       Timex.now()
-      |> Timex.shift(days: 2)
+      |> Timex.shift(days)
       |> Timex.format("{YYYY}-{0M}-{0D}T{0h12}:{m}")
 
     start_date_picker
   end
 
-  defp start_date() do
+  defp set_date(days) do
     {:ok, start_date} =
       Timex.now()
-      |> Timex.shift(days: 2)
+      |> Timex.shift(days)
       |> Timex.format("{YYYY}-{0M}-{0D} {0h12}:{m}:{s}Z")
 
     start_date
-  end
-
-  defp end_date_picker() do
-    {:ok, end_date_picker} =
-      Timex.now()
-      |> Timex.shift(days: 3)
-      |> Timex.format("{YYYY}-{0M}-{0D}T{0h12}:{m}")
-
-    end_date_picker
-  end
-
-  defp end_date() do
-    {:ok, end_date} =
-      Timex.now()
-      |> Timex.shift(days: 3)
-      |> Timex.format("{YYYY}-{0M}-{0D} {0h12}:{m}:{s}Z")
-
-    end_date
   end
 end
