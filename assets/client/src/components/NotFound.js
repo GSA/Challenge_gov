180d0497368372f495e0cc7ecbf5187f4453c608
Stephanie Vizzi <stephanie@smartlogic.io>
2021-11-09 11:34:32 -0500

    React class -> className and mobile spacing CSS

--- assets/client/src/components/NotFound.js
+++ assets/client/src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div class="container p-5">
+    <div className="container p-5">
       <h1>404 / Page not found</h1>
       <span>Please review the URL above and try again, or return to the homepage to search for <a href={location.pathname}>challenges</a>.</span>
     </div>
