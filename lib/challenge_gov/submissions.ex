58ad579f8ec3aa2db03432614ac0d02f9db32f59
Stephanie Vizzi <stephanie@smartlogic.io>
2021-11-29 22:00:07 -0500

    Create an id specific function head for submissions order_on_attribute

--- lib/challenge_gov/submissions.ex
+++ lib/challenge_gov/submissions.ex
@@ -571,6 +571,26 @@ defmodule ChallengeGov.Submissions do
     end
   end
 
+  def order_on_attribute(query, sort_columns = %{"id" => _id}) do
+    columns_to_sort =
+      Enum.reduce(sort_columns, [], fn {column, direction}, acc ->
+        column = String.to_atom(column)
+
+        case direction do
+          "asc" ->
+            acc ++ [asc_nulls_last: column]
+
+          "desc" ->
+            acc ++ [desc_nulls_last: column]
+
+          _ ->
+            []
+        end
+      end)
+
+    order_by(query, [c], ^columns_to_sort)
+  end
+
   def order_on_attribute(query, sort_columns)
       when is_map(sort_columns) and map_size(sort_columns) > 0 do
     %{direction: direction, column: column} =
