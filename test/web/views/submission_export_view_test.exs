cbe4922535d99d63aa933368a7920158832f1faf
Jerrad Christian <jerradchristian@gmail.com>
2022-02-04 10:51:04 -0500

    test explicitly states intent with more robust use cases

--- test/web/views/submission_export_view_test.exs
+++ test/web/views/submission_export_view_test.exs
@@ -14,13 +14,31 @@ defmodule Web.SubmissionExportViewTest do
 
     test "returns appropriate characters when given improperly encoded characters within other strings" do
       improperly_encoded_list = [
-        "Hello&quote; Thereâ€œ I&#x27; think youâ€™re greatâ€“  â€  &#x2F;  &#x2F;  &amp;  Â  "
+        "These are in the database and this test as improperly encoded characters.
+         This is a double quote -> &quote;, This is a different double quote in a word -> Heâ€œllo!,
+         This is a yet another double quote in a word -> Heâ€llo!,
+         This is an apostrophe -> &#x27;, This is a different apostraphe in a word -> Heâ€™llo!,
+         This is a hyphen -> â€“, This is a hyphen in a word -> Heâ€“llo!,
+         This is a forward slash -> &#x2F;, This is a forward slash in a word -> He&#x2F;llo!,
+         This is an ampersand -> &amp;, This is an ampersand in a word -> He&amp;llo!,
+         This is a blank space (two technically) -> Â, This is a blank space in a word -> HeÂllo!
+         --The End--"
       ]
 
       result =
         SubmissionExportView.remove_improperly_encoded_characters(improperly_encoded_list, [])
 
-      assert result == ["Hello\" There\" I' think you're great-  \"  /  /  &     "]
+      assert result == [
+               "These are in the database and this test as improperly encoded characters.
+         This is a double quote -> \", This is a different double quote in a word -> He\"llo!,
+         This is a yet another double quote in a word -> He\"llo!,
+         This is an apostrophe -> ', This is a different apostraphe in a word -> He'llo!,
+         This is a hyphen -> -, This is a hyphen in a word -> He-llo!,
+         This is a forward slash -> /, This is a forward slash in a word -> He/llo!,
+         This is an ampersand -> &, This is an ampersand in a word -> He&llo!,
+         This is a blank space (two technically) ->  , This is a blank space in a word -> He llo!
+         --The End--"
+             ]
     end
   end
 end
