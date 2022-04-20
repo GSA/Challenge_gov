ae3961d1316636ed829d1c41f37d030118f05e4b
Joe Morris <joe@morriscentral.com>
2022-04-20 08:39:03 -0400

    Enable logout with LoginGov

--- lib/web/controllers/session_controller.ex
+++ lib/web/controllers/session_controller.ex
@@ -93,19 +93,15 @@ defmodule Web.SessionController do
     Application.get_env(:challenge_gov, :oidc_config)
   end
 
-  def delete(conn, _params) do
-    %{current_user: user} = conn.assigns
-    Accounts.update_active_session(user, false)
+  def delete(conn = %{assigns: %{current_user: user}}, _params) do
+    user_token = get_session(conn, :user_token)
 
-    SecurityLogs.log_session_duration(
-      user,
-      Timex.to_unix(Timex.now()),
-      Security.extract_remote_ip(conn)
-    )
+    Accounts.update_active_session(user, false)
+    log_session_duration(conn, user)
 
     conn
     |> clear_session()
-    |> redirect(to: Routes.session_path(conn, :new))
+    |> redirect(external: LoginGov.logout_uri(user_token))
   end
 
   @doc """
@@ -151,15 +147,10 @@ defmodule Web.SessionController do
     end
   end
 
-  def logout_user(conn) do
-    %{current_user: user} = conn.assigns
+  def logout_user(conn = %{assigns: %{current_user: user}}) do
     Accounts.update_active_session(user, false)
 
-    SecurityLogs.log_session_duration(
-      user,
-      Timex.to_unix(Timex.now()),
-      Security.extract_remote_ip(conn)
-    )
+    log_session_duration(conn, user)
 
     conn
     |> clear_session()
@@ -174,5 +165,13 @@ defmodule Web.SessionController do
 
   def new_session_timeout_at(timeout_after_minutes) do
     now() + timeout_after_minutes * 60
+  end
+
+  defp log_session_duration(conn, user) do
+    SecurityLogs.log_session_duration(
+      user,
+      Timex.to_unix(Timex.now()),
+      Security.extract_remote_ip(conn)
+    )
   end
 end
