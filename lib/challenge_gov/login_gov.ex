8c6fd4b93da281bc068996a38a7a52c7c142734f
Jerrad Christian <jerradchristian@gmail.com>
2022-07-21 17:11:15 -0400

    Adjust body process for httpoison login_gov.ex

--- lib/challenge_gov/login_gov.ex
+++ lib/challenge_gov/login_gov.ex
@@ -33,7 +33,7 @@ defmodule ChallengeGov.LoginGov do
     }
 
     token_endpoint
-    |> post(body, [{"Content-Type", "application/json"}])
+    |> post(Poison.encode!(body), [{"Content-Type", "application/json"}])
     |> handle_response("Sorry, could not exchange code")
   end
 
@@ -121,10 +121,6 @@ defmodule ChallengeGov.LoginGov do
     uri
     |> URI.merge(path)
     |> URI.to_string()
-  end
-
-  def process_request_body(body) do
-    Poison.encode!(body)
   end
 
   def process_response_body(body) do
