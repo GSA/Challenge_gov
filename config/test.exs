cf1cdc00ec8dfd9eb7bfb9369eab97c5a8d27c20
Alejandro Donis <alejandro.donis@gmail.com>
2022-10-23 21:52:39 -0400

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :challenge_gov, Web.Endpoint,
-  http: [port: 4000],
+  http: [port: 4002],
   server: false
 
 # Print only warnings and errors during test
