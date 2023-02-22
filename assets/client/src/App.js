b605877427cb2e6d3b5da2c25456f2f410ec85db
jdonis <alejandro.donis@gmail.com>
2023-02-22 00:31:11 -0500

    restoring App files

--- assets/client/src/App.js
+++ assets/client/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css';
 import { LandingPage } from './pages/LandingPage'
-import { HelmetProvider } from 'react-helmet-async';
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <HelmetProvider>
       <LandingPage />
-      </HelmetProvider>
     </div>
   );
 }
