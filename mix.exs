73c769901fcf08cc24c55bb3648623cc52880d40
jdonis <alejandro.donis@gmail.com>
2023-03-13 18:06:44 -0400

    Dependencies upgrade

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule ChallengeGov.MixProject do
       {:export, "~> 0.1.1"},
       {:finch, "~> 0.14.0"},
       {:gettext, "~> 0.11"},
-      {:hackney, "~> 1.16.0"},
+      {:hackney, "~> 1.18.0"},
       {:httpoison, "~> 1.7"},
       {:html_sanitize_ex, "~> 1.3.0-rc3"},
       {:jason, "~> 1.0"},
