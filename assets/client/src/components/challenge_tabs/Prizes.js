26959618f338db605e78cf10c05b32f8b4c496e7
jdonis <alejandro.donis@gmail.com>
2023-03-26 19:21:27 -0400

    CHAL-1425: Update challenge UX

--- assets/client/src/components/challenge_tabs/Prizes.js
+++ assets/client/src/components/challenge_tabs/Prizes.js
@@ -8,6 +8,7 @@ export const Prizes = ({challenge, print}) => {
     <ChallengeTab label="Prizes" downloadsLabel="Additional prize documents" section="prizes" challenge={challenge} print={print}>
       {((challenge.prize_type === "monetary" || challenge.prize_type === "both") && challenge.prize_total > 0) &&
         <div className="card">
+           <h5 class="card-title">Total Cash Prizes</h5>
           <div className="card-body">
             <NumberFormat className="ql-editor" value={challenge.prize_total/100} displayType={'text'} thousandSeparator={true} prefix={'$'} />
           </div>
@@ -15,6 +16,7 @@ export const Prizes = ({challenge, print}) => {
       }
       {((challenge.prize_type === "non_monetary" || challenge.prize_type === "both") && challenge.non_monetary_prizes) &&
         <div className="card">
+          <h5 class="card-title">Non-monetary Prizes</h5>
           <div className="card-body">
             <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.non_monetary_prizes }}></div>
           </div>
@@ -22,6 +24,7 @@ export const Prizes = ({challenge, print}) => {
       }
       {challenge.prize_description &&
         <div className="card">
+          <h5 class="card-title">Prize Description</h5>
           <div className="card-body">
             <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.prize_description }}></div>
           </div>
--- assets/client/src/components/challenge_tabs/Rules.js
+++ assets/client/src/components/challenge_tabs/Rules.js
@@ -6,18 +6,21 @@ export const Rules = ({challenge, print}) => {
     <ChallengeTab label="Rules" downloadsLabel="Additional rule documents" section="rules" challenge={challenge} print={print}>
       {challenge.eligibility_requirements &&
         <div className="card">
+          <h5 class="card-title">Eligibility Requirements</h5>
           <div className="card-body">
             <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.eligibility_requirements }}></div>
           </div>
         </div>
       }
       <div className="card">
+        <h5 class="card-title">Official Rules</h5>
         <div className="card-body">
           <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.rules }}></div>
         </div>
       </div>
       {!challenge.terms_equal_rules &&
         <div className="card">
+          <h5 class="card-title">Terms And Conditions</h5>
           <div className="card-body">
             <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.terms_and_conditions }}></div>
           </div>
