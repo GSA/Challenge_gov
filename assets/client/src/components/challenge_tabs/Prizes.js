932bfa1b6adb4329f33f633f47c5cf6126d34e2e
Alejandro Donis <alejandro.donis@gmail.com>
2023-03-27 11:47:03 -0400

    Jdonis/chal 1425 challenge ux (#916)
    * CHAL-1425: Update challenge UX
    * CHAL-1425: Update padding for tabs title
    * CHAL-1425 Modifications after QA for UX enhancements

--- assets/client/src/components/challenge_tabs/Prizes.js
+++ assets/client/src/components/challenge_tabs/Prizes.js
@@ -7,28 +7,37 @@ export const Prizes = ({challenge, print}) => {
   return (
     <ChallengeTab label="Prizes" downloadsLabel="Additional prize documents" section="prizes" challenge={challenge} print={print}>
       {((challenge.prize_type === "monetary" || challenge.prize_type === "both") && challenge.prize_total > 0) &&
-        <div className="card">
-           <h5 class="p-4 card-title">Total Cash Prizes</h5>
-          <div className="card-body">
-            <NumberFormat className="ql-editor" value={challenge.prize_total/100} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+        <span>
+          <h5 class="m-3"><b>Total cash prizes</b></h5>
+          <div className="card">
+            <div className="card-body">
+              <NumberFormat className="ql-editor" value={challenge.prize_total/100} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+            </div>
           </div>
-        </div>
+        </span>
       }
       {((challenge.prize_type === "non_monetary" || challenge.prize_type === "both") && challenge.non_monetary_prizes) &&
+
+        <span>
+        <h5 class="m-3"><b>Non-monetary prizes</b></h5>
         <div className="card">
           <h5 class="p-4 card-title">Non-monetary Prizes</h5>
           <div className="card-body">
             <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.non_monetary_prizes }}></div>
           </div>
         </div>
+        </span>
       }
       {challenge.prize_description &&
+        <span>
+        <h5 class="m-3"><b>Prize description</b></h5>
         <div className="card">
           <h5 class="p-4 card-title">Prize Description</h5>
           <div className="card-body">
             <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.prize_description }}></div>
           </div>
         </div>
+        </span>
       }
     </ChallengeTab>
   )
