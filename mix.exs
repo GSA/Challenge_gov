017644775a51b1e6a09e619badca8fe4dafc1771
jdonis <alejandro.donis@gmail.com>
2023-04-28 23:12:19 -0400

    dependencies

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule ChallengeGov.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies .
+  # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
