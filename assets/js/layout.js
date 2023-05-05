13091314a6fd1e82849fac7472a45b7828629c1d
jdonis <alejandro.donis@gmail.com>
2023-05-05 17:51:53 -0400

    CHAL-1426: upgrade USWSD to 2.14

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,7 +2,7 @@
 // The general page layout will be generally unused in practice because of the react app embed on federalist
 
 // USWDS Import
-import "uswds/dist/scss/uswds.scss";
-import "uswds/dist/js/uswds.js";
+import "/node_modules/uswds/dist/scss/uswds.scss";
+import "/node_modules/uswds/dist/js/uswds.js";
 
 require("../css/layout/index.scss");
