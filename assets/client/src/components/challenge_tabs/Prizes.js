3a0c7f35974ec0b147518867497234de0ea3abf2
Alex Petty <alexander.petty@gmail.com>
2023-06-05 10:01:49 -0400

    Apetty/chal 1152 portal prod amp 20230605 001 (#983)
    * Adjusted 'copy share link' so that it only appears on the challenge detail 'overview' tab to help increase the site's accessiblity score
    
    * Changed ul tag on ChallengeTabs.js to a div to help increase accessibility score
    
    * Fixed repeating partner logo and removed title parameters on challenge details page to help increase accessibility score
    
    * Adjusted challenge image alt tag to help increase accessibility score
    
    * Adjusted 'copy share link' so that it only appears on the challenge detail 'overview' tab to help increase the site's accessiblity score
    
    * Changed ul tag on ChallengeTabs.js to a div to help increase accessibility score
    
    * Fixed repeating partner logo and removed title parameters on challenge details page to help increase accessibility score
    
    * Adjusted challenge image alt tag to help increase accessibility score

--- assets/client/src/components/challenge_tabs/Prizes.js
+++ assets/client/src/components/challenge_tabs/Prizes.js
@@ -8,7 +8,7 @@ export const Prizes = ({challenge, print}) => {
     <ChallengeTab label="Prizes" downloadsLabel="Additional prize documents" section="prizes" challenge={challenge} print={print}>
       {((challenge.prize_type === "monetary" || challenge.prize_type === "both") && challenge.prize_total > 0) &&
         <span>
-          <h5 class="m-3"><b>Total cash prizes</b></h5>
+          <h2 class="m-3"><b>Total cash prizes</b></h2>
           <div className="card">
             <div className="card-body">
               <NumberFormat className="ql-editor" value={challenge.prize_total/100} displayType={'text'} thousandSeparator={true} prefix={'$'} />
@@ -19,7 +19,7 @@ export const Prizes = ({challenge, print}) => {
       {((challenge.prize_type === "non_monetary" || challenge.prize_type === "both") && challenge.non_monetary_prizes) &&
 
         <span>
-        <h5 class="m-3"><b>Non-monetary prizes</b></h5>
+        <h2 class="m-3"><b>Non-monetary prizes</b></h2>
         <div className="card">
           <div className="card-body">
             <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.non_monetary_prizes }}></div>
@@ -29,7 +29,7 @@ export const Prizes = ({challenge, print}) => {
       }
       {challenge.prize_description &&
         <span>
-        <h5 class="m-3"><b>Prize description</b></h5>
+        <h2 class="m-3"><b>Prize description</b></h2>
         <div className="card">
           <div className="card-body">
             <div className="ql-editor" dangerouslySetInnerHTML={{ __html: challenge.prize_description }}></div>
