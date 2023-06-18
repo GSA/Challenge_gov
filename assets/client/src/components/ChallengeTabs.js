59f8ab335828ddc6b63af70a9fde6cd37c9004ff
Alex Petty <alexander.petty@gmail.com>
2023-06-18 14:26:18 -0400

    Make details tabs enter able for accessibility

--- assets/client/src/components/ChallengeTabs.js
+++ assets/client/src/components/ChallengeTabs.js
@@ -64,17 +64,24 @@ export const ChallengeTabs = (props) => {
             const titleCasedLabel = label === "faq" ? label.toUpperCase() :
               label[0].toUpperCase() + label.slice(1).toLowerCase();
             return (
-              <div {...tabProps(label, disabled)}
-                  onClick={(e) => handleTabClick(label, disabled)}>
+              <div
+                {...tabProps(label, disabled)}
+                onClick={(e) => handleTabClick(label, disabled)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' || e.keyCode === 13) {
+                    handleTabClick(label, disabled);
+                  }
+                }} // Add onKeyDown event listener
+              >
                 {renderWinnersIcon(label, disabled)}
                 {titleCasedLabel}
               </div>
-            )
+            );
           }
         })}
       </div>
-    )
-  }
+    );
+  };
 
   const renderTabContent = () => {
     return (
