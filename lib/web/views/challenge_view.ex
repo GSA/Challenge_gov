ffab02184f0051254c3cc0ad25c67f419afe2d04
jdonis <alejandro.donis@gmail.com>
2023-07-18 18:27:09 -0400

    Mix Format

--- lib/web/views/challenge_view.ex
+++ lib/web/views/challenge_view.ex
@@ -118,6 +118,7 @@ defmodule Web.ChallengeView do
     if (user.role == "challenge_manager" or
           Accounts.has_admin_access?(user)) and length(challenge.phases) > 0 do
       link_location = manage_submissions_initial_path(conn, challenge)
+
       content_tag :div do
         link(
           opts[:label] || "View submissions",
