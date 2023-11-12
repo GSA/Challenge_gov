df641a9fa27caa3c0e6b7460b2bff9c8f58a595b
Alex Petty <alexander.petty@gmail.com>
2023-11-12 10:34:48 -0500

    Add new fields to Challenge schema via migrations
    Include submission_collection_method in challenges to specify how submissions are collected.
    
    Add file_upload_required boolean field to determine if file uploads are necessary.
    
    Insert upload_instruction_note field to provide file upload guidance to users.

---
+++ priv/repo/migrations/20231112151006_add_submission_collection_method_to_challenges.exs
@@ -0,0 +1,9 @@
+defmodule ChallengeGov.Repo.Migrations.AddSubmissionCollectionMethodToChallenges do
+ use Ecto.Migration
+
+ def change do
+   alter table(:challenges) do
+     add :submission_collection_method, :string
+   end
+ end
+end
---
+++ priv/repo/migrations/20231112151017_add_file_upload_required_boolean_to_challenges.exs
@@ -0,0 +1,9 @@
+defmodule ChallengeGov.Repo.Migrations.AddFileUploadRequiredBooleanToChallenges do
+ use Ecto.Migration
+
+ def change do
+   alter table(:challenges) do
+     add :file_upload_required, :boolean, default: false
+   end
+ end
+end
