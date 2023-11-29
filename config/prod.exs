b8435902dc2f968916eabd61a9a3602b8b2fbb8f
jdonis <alejandro.donis@gmail.com>
2023-11-29 01:32:28 -0500

    fixing vars

--- config/prod.exs
+++ config/prod.exs
@@ -85,7 +85,7 @@ config :challenge_gov, :oidc_config, %{
   private_key_password: System.get_env("LOGIN_PRIVATE_KEY_PASSWORD"),
   public_key_path: System.get_env("LOGIN_PUBLIC_KEY_PATH"),
   token_endpoint: System.get_env("LOGIN_TOKEN_ENDPOINT"),
-  idp_https_proxy: System.get_env("http_proxy_id"),
+  http_proxy: System.get_env("http_proxy_id"),
   http_port: System.get_env("http_port"),
   http_proxy_user: System.get_env("http_proxy_user"),
   http_proxy_pass: System.get_env("http_proxy_pass")
