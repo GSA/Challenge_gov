b433ddda373aa48402bfafc36e2a1f4c1c43eaec
jdonis <alejandro.donis@gmail.com>
2023-11-30 10:32:35 -0500

    adjust options

--- lib/challenge_gov/login_gov.ex
+++ lib/challenge_gov/login_gov.ex
@@ -147,9 +147,13 @@ defmodule ChallengeGov.LoginGov do
     #     61_443}}
     # ]
 
+    # [
+    #   {:proxy,
+    #    "https://#{Application.get_env(:challenge_gov, :http_proxy_user)}:#{Application.get_env(:challenge_gov, :http_proxy_pass)}@#{Application.get_env(:challenge_gov, :http_proxy)}:61443"}
+    # ]
     [
       {:proxy,
-       "https://#{Application.get_env(:challenge_gov, :http_proxy_user)}:#{Application.get_env(:challenge_gov, :http_proxy_pass)}@#{Application.get_env(:challenge_gov, :http_proxy)}:61443"}
+       "https://0a46f47c-f501-495d-b615-4fbb5cfaa536:JaE9Ti0EttyeX9CkaqvGiq1XF+PP80YO@challengecproxy.apps.internal:61443"}
     ]
   end
 
