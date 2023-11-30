d8ddd076f11ba4d53729d7e190521f733e487ded
jdonis <alejandro.donis@gmail.com>
2023-11-29 19:16:16 -0500

    adjust options

--- lib/challenge_gov/login_gov.ex
+++ lib/challenge_gov/login_gov.ex
@@ -138,8 +138,8 @@ defmodule ChallengeGov.LoginGov do
 
   def process_request_options(options) do
     [
-      {:proxy, {:socks5, "#{Application.get_env(:challenge_gov, :http_proxy)}", 61_443}},
-      {:proxy_auth,
+      {proxy, {"#{Application.get_env(:challenge_gov, :http_proxy)}", 61_443}},
+      {proxy_auth,
        {Application.get_env(:challenge_gov, :http_proxy_user),
         Application.get_env(:challenge_gov, :http_proxy_pass)}}
     ]
