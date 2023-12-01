b58eb6cab4773243f62b1d5123750024f583e14a
jdonis <alejandro.donis@gmail.com>
2023-12-01 16:55:15 -0500

    adjust options

--- lib/challenge_gov/login_gov.ex
+++ lib/challenge_gov/login_gov.ex
@@ -17,7 +17,6 @@ defmodule ChallengeGov.LoginGov do
       {:proxy,
        "https://0a46f47c-f501-495d-b615-4fbb5cfaa536:JaE9Ti0EttyeX9CkaqvGiq1XF+PP80YO@challengecproxy.apps.internal:61443"}
     ])
-
   end
 
   def get_public_key(jwks_uri) do
