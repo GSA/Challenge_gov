3c6b132756f29de3679d72db4e6d0bf8bbfcabb1
jdonis <alejandro.donis@gmail.com>
2024-02-08 16:47:36 -0500

    remove https

--- lib/challenge_gov/login_gov.ex
+++ lib/challenge_gov/login_gov.ex
@@ -125,7 +125,7 @@ defmodule ChallengeGov.LoginGov do
 
   def process_request_options(options) do
     [
-      proxy: {"https://challengecproxy.apps.internal:61443"},
+      proxy: {"challengecproxy.apps.internal", 61_443},
       ssl: [
         verify: :verify_none
       ]
