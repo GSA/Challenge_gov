9d6ed5f07be2d01d9de665c0193329ff77f5ba76
jdonis <alejandro.donis@gmail.com>
2024-02-09 13:39:37 -0500

    port+link

--- lib/challenge_gov/login_gov.ex
+++ lib/challenge_gov/login_gov.ex
@@ -126,7 +126,7 @@ defmodule ChallengeGov.LoginGov do
 
   def process_request_options(options) do
     [
-      proxy: {"https://challengecproxy.apps.internal:61443"},
+      proxy: {"https://challengecproxy.apps.internal", 61_443},
       ssl: [verify: :verify_none],
       hackney: [
         insecure: true
