898e6882def7309bdd7346fdcb52df0c5715636d
jdonis <alejandro.donis@gmail.com>
2024-06-04 08:23:50 -0400

    webpack configuration

--- assets/js/app/_sub_agency_select.js
+++ assets/js/app/_sub_agency_select.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import fetch from 'node-fetch'
 
 $("select#challenge_agency_id").on("change", (e) => {
@@ -8,23 +9,25 @@ $("#js-federal-partners").on("change", ".js-agency-select", (e) => {
   addSubAgencyOptions(e.target.value, $(e.target).parents(".form-collection").find(".js-sub-agency-select"))
 })
 
-function addSubAgencyOptions(agency_id, sub_agency_select) {
+async function addSubAgencyOptions(agency_id, sub_agency_select) {
 
-  fetch(`/api/agencies/${agency_id}/sub_agencies`)
-  .then((res) => {
+ try {
+
+    const response = await fetch(`/api/agencies/${agency_id}/sub_agencies`);
+    const res = await response.text();
     let sub_agencies = res.data
     let sub_agency_options = sub_agencies.map((sub_agency) => {
       return `<option value=${sub_agency.id}>${sub_agency.name}</option>`
     })
-
+    
     sub_agency_select.val(null)
-
     sub_agency_select.html(["<option value=''>Choose a sub-agency</option>"].concat(sub_agency_options))
-  })
-  .catch((e) => {
-    console.log("Error fetching sub agencies", e)
-  })
 
+ } catch (e) {
+    console.log("Error fetching sub agencies", e)
+ }
+
+  
 
   // axios
   //   .get(`/api/agencies/${agency_id}/sub_agencies`)
@@ -41,5 +44,7 @@ function addSubAgencyOptions(agency_id, sub_agency_select) {
   //   .catch(e => {
   //     console.log("Error fetching sub agencies", e)
   //   })
+
+
 
 }
