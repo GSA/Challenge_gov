9bae3f1cda3cca5d3a86078fda6898b9d2490c04
Jay X Anaya <jairo.anaya@gmail.com>
2024-11-04 22:18:32 -0500

    Fix error on changing test email

--- test/integration/challenge_test.exs
+++ test/integration/challenge_test.exs
@@ -159,7 +159,7 @@ defmodule ChallengeGov.ChallengeIntegrationTest do
 
   defp create_and_sign_in_challenge_manager(session) do
     AccountHelpers.create_user(%{
-      email: "challenge_manager_active2@example.gov",
+      email: "challenge_manager_active@example.gov",
       role: "challenge_manager"
     })
 
