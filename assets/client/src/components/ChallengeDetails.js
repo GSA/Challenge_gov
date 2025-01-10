ef97770936d282e004585fa3ff5e9d96b0e35dd0
Kenny Krug <kenny.krug@gsa.gov>
2025-01-10 14:41:17 -0500

    Agency Logo Not Displaying on Challenge Page #1503 Source url fix

--- assets/client/src/components/ChallengeDetails.js
+++ assets/client/src/components/ChallengeDetails.js
@@ -350,7 +350,7 @@ export const ChallengeDetails = ({challenge, challengePhases, preview, print, ta
                   <div className="logos">
                     <img
                       className="agency-logo"
-                      src={`${imageBase}${encodeURI(challenge.agency_logo)}`}
+                      src={`${imageBase ? imageBase : ''}${encodeURIComponent(challenge.agency_logo || '')}`}
                       alt={`Agency logo for ${challenge.agency_name}`}                    
                     />
 
