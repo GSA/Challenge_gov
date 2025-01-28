5ade98367b11e9d4c4c417682c6944049a97df6d
jdonis <alejandro.donis@gmail.com>
2025-01-28 11:02:03 -0500

    Db testing

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :challenge_gov, Web.Endpoint,
 
 config :challenge_gov, ChallengeGov.Repo,
   url: System.get_env("DATABASE_URL"),
-  # ssl: true,
+  ssl: true,
   # pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
   pool_size: 15,
   migration_timestamps: [type: :utc_datetime_usec],
