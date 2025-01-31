582de370aa5c2d8cc1596112aba06dcbf3bb59f7
jdonis <alejandro.donis@gmail.com>
2025-01-30 22:32:18 -0500

    ssl opts change 2

--- config/prod.exs
+++ config/prod.exs
@@ -35,8 +35,7 @@ config :challenge_gov, ChallengeGov.Repo,
   #  timeout: :infinity,
   ssl_opts: [
     cacertfile: "priv/certs/us-gov-west-1-bundle.pem",
-    verify: :verify_peer,
-    sslmode: :verify_full,
+    verify: :verify_none,
     versions: [:"tlsv1.2", :"tlsv1.3"]
   ],
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "15"),
