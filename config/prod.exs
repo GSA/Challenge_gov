613ecc844d8717d6d7b7f03b5ab8b297a7f08651
Kenny Krug <kenny.krug@gsa.gov>
2025-02-03 17:00:56 -0500

    Database Engine Version Upgrade #1486 verify_none test

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :challenge_gov, ChallengeGov.Repo,
   ssl: true,
   ssl_opts: [
     cacertfile: "priv/certs/us-gov-west-1-bundle.pem",
-    verify: :verify_peer,
+    verify: :verify_none,
     versions: [:"tlsv1.2", :"tlsv1.3"]
   ],
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "15"),
