47a40d0958a013dba3f778ac35724e31da941ae9
Kenny Krug <kenny.krug@gsa.gov>
2025-02-04 14:41:15 -0500

    Database Engine Version Upgrade #1486 ssl rollback

--- config/prod.exs
+++ config/prod.exs
@@ -18,12 +18,6 @@ config :challenge_gov, Web.Endpoint,
 
 config :challenge_gov, ChallengeGov.Repo,
   url: System.get_env("DATABASE_URL"),
-  ssl: true,
-  ssl_opts: [
-    cacertfile: "priv/certs/us-gov-west-1-bundle.pem",
-    verify: :verify_peer,
-    versions: [:"tlsv1.2", :"tlsv1.3"]
-  ],
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "15"),
   loggers: [{LoggerJSON.Ecto, :log, [:info]}]
 
