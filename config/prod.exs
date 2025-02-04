92ba76cbab899e1a86cee1bce169b9b3f7f4050c
Kenny Krug <kenny.krug@gsa.gov>
2025-02-04 09:26:17 -0500

    Database Engine Version Upgrade #1486 lint fix

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,9 @@ config :challenge_gov, ChallengeGov.Repo,
     cacertfile: "priv/certs/us-gov-west-1-bundle.pem",
     server_name_indication: "cg-aws-broker-prodo0g0lv1irkwn9f3.ci7nkegdizyy.us-gov-west-1.rds.amazonaws.com",
     verify: :verify_peer,
-    verify_fun: {&:ssl_verify_hostname.verify_fun/3, [check_hostname: "cg-aws-broker-prodo0g0lv1irkwn9f3.ci7nkegdizyy.us-gov-west-1.rds.amazonaws.com"]}
+    verify_fun: {&:ssl_verify_hostname.verify_fun/3, [
+      check_hostname: "cg-aws-broker-prodo0g0lv1irkwn9f3.ci7nkegdizyy.us-gov-west-1.rds.amazonaws.com"
+    ]}
   ],
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "15"),
   loggers: [{LoggerJSON.Ecto, :log, [:info]}]
