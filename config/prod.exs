94311ded12d03fd1c21c341896fa93ebb19dcf9d
Kenny Krug <kenny.krug@gsa.gov>
2025-02-04 12:01:11 -0500

    Database Engine Version Upgrade #1486 back to dev ssl config

--- config/prod.exs
+++ config/prod.exs
@@ -16,16 +16,13 @@ config :challenge_gov, Web.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-check_hostname = 'cg-aws-broker-prodo0g0lv1irkwn9f3.ci7nkegdizyy.us-gov-west-1.rds.amazonaws.com'
-
 config :challenge_gov, ChallengeGov.Repo,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
   ssl_opts: [
     cacertfile: "priv/certs/us-gov-west-1-bundle.pem",
-    server_name_indication: check_hostname,
     verify: :verify_peer,
-    verify_fun: {&:ssl_verify_hostname.verify_fun/3, [check_hostname: check_hostname]}
+    versions: [:"tlsv1.2", :"tlsv1.3"]
   ],
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "15"),
   loggers: [{LoggerJSON.Ecto, :log, [:info]}]
